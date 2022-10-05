1dd14aa079d239edc866d448a26869ad6f003379
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-10-05 14:47:19 -0700

    use hash and verify to practice password handling

--- index.php
+++ index.php
@@ -6,7 +6,16 @@
 <body>
 
   <?php
+    // password_hash takes in the password and a method to hash the password
+    // echo "test123";
+    // echo "<br>";
+    // echo password_hash("test123", PASSWORD_DEFAULT);
 
+    $input = "test123";
+    $hashedPwdInDb = password_hash("test123", PASSWORD_DEFAULT);
+    
+    // password_verify takes in the password in the db and the password entered by user, returns 1 or 0 (true or false)
+    echo password_verify($input, $hashedPwdInDb);
   ?>
 
 </body>
