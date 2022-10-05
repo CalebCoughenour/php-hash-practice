855a5eae1600edc14dda589e71e8e2b48dfa28a5
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-10-05 15:08:24 -0700

    begin setting up navbar

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="description" content="Example metadata description. This usually shows in search results">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Mock Title</title>
+</head>
+<body>
+
+
+<header>
+  <nav>
+    <a href="index.php">
+      <img src="img/favicon.png" alt="Navbar logo">
+    </a>
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="#">Portfolio</a></li>
+      <li><a href="#">About Me</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+    <div>
+      <form action="includes/login.inc.php" method="post"></form>
+    </div>
+  </nav>
+</header>
