92446d8ff6cdf10bf17c219452b341e5372d1f73
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-10-06 15:55:28 -0700

    add check for existing email in database

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -42,21 +42,38 @@ if (isset($_POST['signup-submit'])) {
         header("Location: ../signup.php?error=usertaken&mail=".$email."&uid=".$uidUsers);
         exit();
       } else {
-        $sql = "INSERT INTO users (uidUsers, emailUsers, pwdUsers) VALUES (?, ?, ?)";
+
+        $sql = "SELECT emailUsers FROM users WHERE emailUsers=?";
         $stmt = mysqli_stmt_init($conn);
-        if(!mysqli_stmt_prepare($stmt, $sql)) {
+
+        if (!mysqli_stmt_prepare($stmt, $sql)) {
           header("Location: ../signup.php?error=databaseerror&mail=".$email."&uid=".$uidUsers);
           exit();
         } else {
-          $hashedPwd = password_hash($password, PASSWORD_DEFAULT);
-
-          mysqli_stmt_bind_param($stmt, "sss", $username, $email, $hashedPwd);
+          mysqli_stmt_bind_param($stmt, "s", $email);
           mysqli_stmt_execute($stmt);
-          header("Location: ../signup.php?signup=success");
-          exit();
+          mysqli_stmt_store_result($stmt);
+          $emailCheck = mysqli_stmt_num_rows($stmt);
+          if ($emailCheck > 0) {
+            header("Location: ../signup.php?error=emailtaken&mail=".$email."&uid=".$uidUsers);
+            exit();
+          } else {
+            $sql = "INSERT INTO users (uidUsers, emailUsers, pwdUsers) VALUES (?, ?, ?)";
+            $stmt = mysqli_stmt_init($conn);
+            if(!mysqli_stmt_prepare($stmt, $sql)) {
+              header("Location: ../signup.php?error=databaseerror&mail=".$email."&uid=".$uidUsers);
+              exit();
+            } else {
+              $hashedPwd = password_hash($password, PASSWORD_DEFAULT);
+    
+              mysqli_stmt_bind_param($stmt, "sss", $username, $email, $hashedPwd);
+              mysqli_stmt_execute($stmt);
+              header("Location: ../signup.php?signup=success");
+              exit();
+            }
+          }
         }
       }
-
     }
   }
   //close connection to avoid potentially using uneeded resources
