347abc1a326fc933a1dea5caf7626d7823f9afc2
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-10-06 16:03:55 -0700

    add condition to check if session has started to demonstrate logged in/logged out content

--- header.php
+++ header.php
@@ -42,15 +42,20 @@
           </li>
         </ul>
 
-        <form action="includes/login.inc.php" method="post">
-          <input type="text" name="mailuid" placeholder="Username or E-mail">
-          <input type="password" name="pwd" placeholder="Password">
-          <button type="submit" name="login-submit" class="btn btn-sm btn-outline-secondary">Login</button>
-        </form>
-        <a href="signup.php">Signup</a>
-        <form action="includes/logout.inc.php" method="post">
-          <button type="submit" name="logout-submit" class="btn btn-sm btn-outline-secondary">Logout</button>
-        </form>
+        <?php
+        if (isset($_SESSION['userId'])) {
+          echo '<form action="includes/logout.inc.php" method="post">
+                  <button type="submit" name="logout-submit" class="btn btn-sm btn-outline-secondary">Logout</button>
+                </form>';
+        } else {
+          echo '<form action="includes/login.inc.php" method="post">
+                  <input type="text" name="mailuid" placeholder="Username or E-mail">
+                  <input type="password" name="pwd" placeholder="Password">
+                  <button type="submit" name="login-submit" class="btn btn-sm btn-outline-secondary">Login</button>
+                </form>
+                <a href="signup.php">Signup</a>';
+        }
+        ?>
       </div>
     </div>
   </nav>
