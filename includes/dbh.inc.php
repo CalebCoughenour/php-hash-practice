aceb8d761169c1dc5c1f7dd8eddf968664d0e29b
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-10-06 16:01:59 -0700

    add correct server for sqli database

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -5,7 +5,7 @@ $dBUsername = "root";
 $dBPassword = "";
 $dBName = "loginsystem_tut";
 
-$conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
+$conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName, 3307);
 
 if (!$conn) {
   die("Connection failed: ".mysqli_connect_error());
