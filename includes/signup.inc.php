01c70d330003c9d91fe0442c24a35c88623b821e
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-10-06 14:33:06 -0700

    add error handling conditions for sign up

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -7,10 +7,64 @@ if (isset($_POST['signup-submit'])) {
   $username = $_POST['uid'];
   $email = $_POST['mail'];
   $password = $_POST['pwd'];
-  $passwordRepeat = $_POST['pwd-repeat'];
+  $passwordRepeat = $_POST['pwdRepeat'];
 
+  // Error handling firs then handle sign up
   if (empty($username) || empty($email) || empty($password) || empty($passwordRepeat)) {
     header("Location: ../signup.php?error=emptyfields&uid=".$username."&mail=".$email);
-  }
+    exit();
+  } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL) && !preg_match("/^[a-zA-Z0-9]*$/", $username)) {
+    header("Location: ../signup.php?error=invalidusernameuid");
+    exit();
+  } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    header("Location: ../signup.php?error=invalidemail&uid=".$username);
+    exit();
+  } elseif(!preg_match("/^[a-zA-Z0-9]*$/", $username)) {
+    header("Location: ../signup.php?error=invalidusername&mail".$email);
+    exit();
+  } elseif ($password !== $passwordRepeat) {
+    header("Location: ../signup.php?error=passwordcheck&uid=".$username."&mail=".$email);
+    exit();
+  } else {
+    $sql = "SELECT uidUsers FROM users WHERE uidUsers=?";
+    $stmt = mysqli_stmt_init($conn);
 
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+      header("Location: ../signup.php?error=databaseerror&mail=".$email."&uid=".$uidUsers);
+      exit();
+    } else {
+      // Checks database for existing username by checking if any rows are returned
+      mysqli_stmt_bind_param($stmt, "s", $username);
+      mysqli_stmt_execute($stmt);
+      mysqli_stmt_store_result($stmt);
+      $resultCheck = mysqli_stmt_num_rows($stmt);
+      if ($resultCheck > 0) {
+        header("Location: ../signup.php?error=usertaken&mail=".$email."&uid=".$uidUsers);
+        exit();
+      } else {
+        $sql = "INSERT INTO users (uidUsers, emailUsers, pwdUsers) VALUES (?, ?, ?)";
+        $stmt = mysqli_stmt_init($conn);
+        if(!mysqli_stmt_prepare($stmt, $sql)) {
+          header("Location: ../signup.php?error=databaseerror&mail=".$email."&uid=".$uidUsers);
+          exit();
+        } else {
+          $hashedPwd = password_hash($password, PASSWORD_DEFAULT);
+
+          mysqli_stmt_bind_param($stmt, "sss", $username, $email, $hashedPwd);
+          mysqli_stmt_execute($stmt);
+          header("Location: ../signup.php?signup=success");
+          exit();
+        }
+      }
+
+    }
+  }
+  //close connection to avoid potentially using uneeded resources
+  mysqli_stmt_close($stmt);
+  mysqli_close($conn);
+} else {
+  // sends user back to signup page if something goes wrong
+  header("Location: ../signup.php");
+  exit();
 }
+
